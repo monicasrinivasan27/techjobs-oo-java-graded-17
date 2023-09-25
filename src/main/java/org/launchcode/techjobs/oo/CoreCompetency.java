f11d668ead6f83d2bd358043aa8155b589b9223e
monicasrinivasan27 <srinivasan.monica@gmail.com>
2023-09-25 10:17:16 -0500

    Completed Task 2

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -8,6 +8,8 @@ public class CoreCompetency {
     private static int nextId = 1;
     private String value;
 
+
+
     public CoreCompetency() {
         this.id = nextId;
         nextId++;
@@ -40,5 +42,16 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
 
 }
